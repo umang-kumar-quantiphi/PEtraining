eff08d861df9b7965778e6b0bd6245d80d655ca1
Umang Kumar <umang.kumar@quantiphi.com>
2018-05-24 17:37:52 +0530

    third

--- Find the Runner-Up Score.py
+++ Find the Runner-Up Score.py
@@ -1,3 +1,12 @@
 if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    max1=max(arr)
+    max2=-9999999999
+    for x in arr:
+        if(x>max1): 
+            max1=x
+            
+        elif(x>max2 and x<max1):max2=x
+            
+    print max2   
